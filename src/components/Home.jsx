b310353a81dc4985828b9e84c77632fb4d8905cb
nithin <sureshnithin1729@gmail.com>
2024-06-02 05:31:24 +0530

    done for now will get back after a month

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ import index from '../index.css';
 function Home() {
   return (
     <section id="Home" className="home-section">
-      <div className="home-div text-center">
-        <div className="home-div-p1 mb-4">
+      <div className="home-div">
+        <div className="home-div-p1">
           <p>Hello</p>
         </div>
-        <div className="home-div-p2 mb-4">
+        <div className="home-div-p2">
           <p className=''>This is Nithin</p>
         </div>
-        <div className="home-div-p3 mb-4">
+        <div className="home-div-p3">
         <p>IT undergrad at NITK'26</p>
         </div>
       </div>
