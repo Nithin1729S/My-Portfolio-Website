3259cb435b13a4345575e3ae86be8b044e073d98
nithin <sureshnithin1729@gmail.com>
2024-06-23 00:56:07 +0530

    restructured experience data

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ExpCard from '../Cards/ExpCard';
-import isteLogo from '../components/images/istenitk.png';
-import ieeeLogo from '../components/images/ieee.png';
-import haleLogo from '../components/images/HALE.png';
 import { useState, useEffect } from 'react';
-
+import experience from './data/experience'
 
 function Experience() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -19,35 +16,6 @@ function Experience() {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-  const experience=[
-    {
-      id:'1',
-      imgSrc:isteLogo,
-      Where:"ISTE NITK",
-      Time:"Nov 2022 - Present",
-      Position:"Executive Member",
-      WorkDone1:"-Manage official website of ISTE NITK",
-      WorkDone2:"-Developed ISTE's Virtual Expo"
-    },
-    {
-      id:'2',
-      imgSrc:haleLogo,
-      Where:"HALE Labs NITK",
-      Time:"April 2024 - Present",
-      Position:"Research Intern",
-      WorkDone1:"-Manage official website of ISTE NITK",
-      WorkDone2:"-Developed ISTE's Virtual Expo"
-    },
-    {
-      id:'3',
-      imgSrc:ieeeLogo,
-      Where:"IEEE NITK",
-      Time:"Nov 2022 - April 2023",
-      Position:"Student Member",
-      WorkDone1:"-Manage official website of ISTE NITK",
-      WorkDone2:"-Developed ISTE's Virtual Expo"
-    },
-  ]
 
   const MobileExp = () => (
     <>
---
+++ src/components/data/experience.jsx
@@ -0,0 +1,35 @@
+import isteLogo from '../images/istenitk.png';
+import ieeeLogo from '../images/ieee.png';
+import haleLogo from '../images/HALE.png';
+
+const experience=[
+    {
+      id:'1',
+      imgSrc:isteLogo,
+      Where:"ISTE NITK",
+      Time:"Nov 2022 - Present",
+      Position:"Executive Member",
+      WorkDone1:"-Manage official website of ISTE NITK",
+      WorkDone2:"-Developed ISTE's Virtual Expo"
+    },
+    {
+      id:'2',
+      imgSrc:haleLogo,
+      Where:"HALE Labs NITK",
+      Time:"April 2024 - Present",
+      Position:"Research Intern",
+      WorkDone1:"-Manage official website of ISTE NITK",
+      WorkDone2:"-Developed ISTE's Virtual Expo"
+    },
+    {
+      id:'3',
+      imgSrc:ieeeLogo,
+      Where:"IEEE NITK",
+      Time:"Nov 2022 - April 2023",
+      Position:"Student Member",
+      WorkDone1:"-Manage official website of ISTE NITK",
+      WorkDone2:"-Developed ISTE's Virtual Expo"
+    },
+  ]
+
+  export default experience;
