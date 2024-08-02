cca0e205783c5c64d327af886d05afe9356ce740
nithin <sureshnithin1729@gmail.com>
2024-08-03 01:44:09 +0530

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar(props) {
         <ul className="nav nav-pills col col-sm-3 col-md-6 col-lg-5 justify-content-center">
           <li className="nav-item ">
             <div className='Home'>
-              <a className="nav-link link-dark" href="https://drive.google.com/file/d/1H7ZaD7siI3BBSeEX08JqgOf1RNmICwZw/view?usp=sharing" target="_blank" rel="noreferrer">              <span className="border border-dark transition duration-300 ease-in-out hover:border-gray-700 hover:text-white hover:bg-black hover:scale-105">Resume</span>
+              <a className="nav-link link-dark" href="https://drive.google.com/file/d/1dWWrcmYYM3mYKfBo0T1LHops4c_s3Csv/view?usp=sharing" target="_blank" rel="noreferrer">              <span className="border border-dark transition duration-300 ease-in-out hover:border-gray-700 hover:text-white hover:bg-black hover:scale-105">Resume</span>
               </a>
             </div>
           </li>
